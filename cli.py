a60ea39b8f9422864642840858211300aadcd760
“Rishi” <rishiraj.mullur1997@gmail.com>
2023-11-14 23:46:18 -0800

    refactored code to support new class

--- cli.py
+++ cli.py
@@ -1,40 +1,45 @@
-# This file contains the Command Line Interface (CLI) for
-# the Tic-Tac-Toe game. This is where input and output happens.
-# For core game logic, see logic.py.
-
-from logic import make_empty_board, get_winner, show_current_board ,update_board, other_player
 import logging
+from logic import TicTacToeLogic
+
+class TicTacToeCLI:
+    def __init__(self, logic):
+        self.logic = logic
+
+    def play_game(self):
+        board = self.logic.make_empty_board()
+        winner = None
+        char = 'X'
+        logging.basicConfig(filename='logs/game.log', format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', encoding='utf-8', level=logging.DEBUG)
+        logging.info('New Game begin')
+        print("Choose Type of Game: 1. Single Player 2.Two-Player")
+        game_type = int(input("Option:"))
+        if game_type == 1:
+            print("Single Player Mode selected!")
+        elif game_type == 2:
+            print("Two Player Mode Selected!")
+
+        while winner is None:
+            print("-------------------------------------")
+            logging.info("Turn Begins")
+            print("Begin Turn!")
+            print("It's", char, "turn!")
+            self.logic.show_current_board(board=board)
+            print("Enter the row and column of the board you want to add the move to:")
+            i = int(input("Enter the row number (Options- 1 | 2 | 3):"))
+            j = int(input("Enter the column (Options- 1 | 2 | 3):"))
+            board, is_illegal = self.logic.update_board(board=board, i=i, j=j, char=char)
+            # if an illegal move happens, do not change the turn
+            if not is_illegal:
+                char = self.logic.other_player(player=char)
+            winner = self.logic.get_winner(board)
+            if winner == 'Draw':
+                print("Game Draw")
+            else:
+                logging.info("Winner")
+
 
 if __name__ == '__main__':
-    board = make_empty_board()
-    winner = None
-    # Intiazlize the first move to X
-    char = 'X'
-    logging.basicConfig(filename='logs/game.log', format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p' ,encoding='utf-8', level=logging.DEBUG)
-    logging.info('New Game begin')
-        print("Choose Type of Game: 1. Single Player 2.Two-Player")
-    game_type=int(input("Option:"))
-    if game_type==1:
-        print("Single Player Mode selected!")
-    elif game_type==2:
-        print("Two Player Mode Selected!")
-
-    while winner == None:
-        print("-------------------------------------")
-        logging.info("Turn Begins")
-        print("Begin Turn!")
-        print("It's", char, "turn!")
-        show_current_board(board=board)
-        print("Enter the row and column of the board you want to add the move to:")
-        i = int(input("Enter the row number (Options- 1 | 2 | 3):"))
-        j = int(input("Enter the column (Options- 1 | 2 | 3):"))
-        board, is_illegal = update_board(board=board,i=i,j=j,char=char)
-        #if an illegal move happens, do not change the turn
-        if is_illegal!= True:
-            char = other_player(player = char)
-        winner = get_winner(board)
-        if winner == 'Draw':
-            print("Game Draw")
-        else:
-            logging.info("Winner")
+    logic = TicTacToeLogic()
+    cli = TicTacToeCLI(logic)
+    cli.play_game()
 
--- logic.py
+++ logic.py
@@ -3,83 +3,87 @@
 # should be unit-testable.
 
 
-def make_empty_board():
-    return [
-        [None, None, None],
-        [None, None, None],
-        [None, None, None],
-    ]
+class TicTacToeLogic:
+    def __init__(self):
+        pass
+
+    def make_empty_board(self):
+        return [
+            [None, None, None],
+            [None, None, None],
+            [None, None, None],
+        ]
 
 # Main logic for the winner of a given board
-def get_winner(board):
-    """Determines the winner of the given board.
-    Returns 'X', 'O', or None."""
-    for i in board:
-        if i[0]==i[1]==i[2]=='X':
+    def get_winner(self,board):
+        """Determines the winner of the given board.
+        Returns 'X', 'O', or None."""
+        for i in board:
+            if i[0]==i[1]==i[2]=='X':
+                print('X won')
+                return 'X'
+            elif i[0]==i[1]==i[2]=='O':
+                print('O won')
+                return 'O'
+
+        for j in range(3):
+        # print(board[0][j])
+            if board[0][j]==board[1][j]==board[2][j]=='X':
+                print('X won')
+                return 'X'
+            elif board[0][j]==board[1][j]==board[j]=='O':
+                print('O won')
+                return 'O'
+
+        if board[0][0]==board[1][1]==board[2][2]=='X':
             print('X won')
             return 'X'
-        elif i[0]==i[1]==i[2]=='O':
+        elif board[0][0]==board[1][1]==board[2][2]=='0':
             print('O won')
             return 'O'
-
-    for j in range(3):
-    # print(board[0][j])
-        if board[0][j]==board[1][j]==board[2][j]=='X':
-            print('X won')
+        
+        if board[0][2] == board[1][1] == board[2][0] == 'X':
             return 'X'
-        elif board[0][j]==board[1][j]==board[j]=='O':
-            print('O won')
+        elif board[0][2] == board[1][1] == board[2][0] == 'O':
             return 'O'
-
-    if board[0][0]==board[1][1]==board[2][2]=='X':
-        print('X won')
-        return 'X'
-    elif board[0][0]==board[1][1]==board[2][2]=='0':
-        print('O won')
-        return 'O'
-    
-    if board[0][2] == board[1][1] == board[2][0] == 'X':
-        return 'X'
-    elif board[0][2] == board[1][1] == board[2][0] == 'O':
-        return 'O'
-    
-    # Check for a draw
-    for row in board:
-        if None in row:
-            return None
-    return 'Draw'  
+        
+        # Check for a draw
+        for row in board:
+            if None in row:
+                return None
+        return 'Draw'  
 
 
-def other_player(player):
-    """Given the character for a player, returns the other player."""
-    if player == 'X':
-        return 'O'
-    else:
-        return 'X'
+    def other_player(self,player):
+        """Given the character for a player, returns the other player."""
+        if player == 'X':
+            return 'O'
+        else:
+            return 'X'
 
 
-def update_board(board,i,j,char):
-    """Updates the playing board to account for the new move of a user"""
-    #Making the row and column accept easier row/column values for users
-    i = i-1
-    j = j-1
-    if i < 0 or j < 0:
-        print("No such row or column")
-        return board
-    if i >=3 or j >=3:
-        print("No such row or column")
-        return board
-    if board[i][j] == None:
-        board[i][j] = char
-        return board, False
-    else:
-        print("Illegal move!")
-        return board, True
+    def update_board(self,board,i,j,char):
+        """Updates the playing board to account for the new move of a user"""
+        #Making the row and column accept easier row/column values for users
+        i = i-1
+        j = j-1
+        if i < 0 or j < 0:
+            print("No such row or column")
+            return board
+        if i >=3 or j >=3:
+            print("No such row or column")
+            return board
+        if board[i][j] == None:
+            board[i][j] = char
+            return board, False
+        else:
+            print("Illegal move!")
+            return board, True
 
 
-def show_current_board(board):
-    print(board[0])
-    print(board[1])
-    print(board[2])
+    def show_current_board(self,board):
+        print(board[0])
+        print(board[1])
+        print(board[2])
 
 
